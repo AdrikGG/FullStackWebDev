f3a00b0340fa0079fd27309f0d98f47a42655dce
Adrik Gurganus <adrik@pdx.edu>
2022-10-05 16:58:57 -0700

    Revert "completed hw1"
    This reverts commit 1ca6970e1b34eb8f35fd5e1b098f7a98a8a816cf.

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -10,17 +10,7 @@ Use console.log() to write the proper output to the command line.
 **/
 
 const fizzbuzz = () => {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 == 0) {
-      console.log("fizzbuzz");
-    } else if (i % 3 === 0) {
-      console.log("fizz");
-    } else if (i % 5 === 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
-    }
-  }
+  // Add your code here
 };
 
 fizzbuzz();
