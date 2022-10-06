f5567817c84cc49766a036937ade0af916ea8c7b
Adrik Gurganus <adrik@pdx.edu>
2022-10-05 17:01:17 -0700

    hw1 completed

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -10,7 +10,17 @@ Use console.log() to write the proper output to the command line.
 **/
 
 const fizzbuzz = () => {
-  // Add your code here
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 == 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 };
 
 fizzbuzz();
