84fd08a77aff58bbb9c90406395d0a87572b92cd
Adrik Gurganus <adrik@pdx.edu>
2022-10-09 23:17:14 -0700

    coins complete, reverse logic done

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,25 @@
 /** Exercise 02 - Reverse **/
 
-// Add your code here
+const input = document.getElementById("input");
+const submit = document.getElementById("reverse");
+
+const reverse = (input) => {
+    if (isNaN(input)) {
+        return "Error: Input must be a number";
+    }
+
+    let length = String(input).replace("-", "").length;
+    if (length !== 8) {
+        return "Error: Please input an 8-digit number";
+    }
+
+    let reversed = 0;
+    let currDigit = 0;
+    while (input) {
+        currDigit = input % 10;
+        reversed = (reversed * 10) + currDigit;
+        input = Math.floor(input / 10);
+    }
+
+    return input + " --> " + reversed;
+}
