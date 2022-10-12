f52624a792f3ec0edd108454d829390dbfe0fcc0
Adrik Gurganus <adrik@pdx.edu>
2022-10-12 00:59:46 -0700

    better form validation

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -2,7 +2,35 @@
 
 const submit = document.getElementById("submit");
 
-submit.onclick = function () {
+submit.onclick = function (e) {
+  e.preventDefault();
+  let ok = true;
+  if (document.getElementById("namefield").value.length === 0) {
+    document.getElementById("namefield").style.borderColor = "red";
+    document.getElementById("nameerror").innerText = "Name is required";
+    document.getElementById("nameerror").hidden = false;
+    ok = false;
+  } else {
+    document.getElementById("namefield").style.borderColor = "#ced4da";
+    document.getElementById("nameerror").hidden = true;
+  }
+  if (document.getElementById("emailfield").value.length === 0) {
+    document.getElementById("emailfield").style.borderColor = "red";
+    document.getElementById("emailerror").innerText = "Email is required";
+    document.getElementById("emailerror").hidden = false;
+    ok = false;
+  } else if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(document.getElementById("emailfield").value)) {
+    document.getElementById("emailfield").style.borderColor = "red";
+    document.getElementById("emailerror").innerText = "Enter a valid email address";
+    document.getElementById("emailerror").hidden = false;
+    ok = false;
+  } else {
+    document.getElementById("emailfield").style.borderColor = "#ced4da";
+    document.getElementById("emailerror").hidden = true;
+  }
+  if (!ok) {
+    return;
+  }
   console.group("========= Form Submission =========");
   console.log("Name: " + document.getElementById("namefield").value);
   console.log("Email: " + document.getElementById("emailfield").value);
